9cdb2a2a0f186d536dc72e0440a69b82be8bf3d7
Sandrine Poissonnet <spoisson@gmail.com>
2021-08-02 07:01:21 -0700

    basic back-end

---
+++ routes/transactions.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactions')
+
+router.route('/').get(getTransactions).post(addTransaction);
+
+router.route('/:id').delete(deleteTransaction);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const transactions = require('./routes/transactions');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+
+app.use(express.json()); // middleware for the body parser
+app.use('/api/v1/transactions', transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
+);
